6b1aab032edd7bd2fe7718d6f1ec3ea1e8fbd21b
CHAMPALAUNE Maël <mael.champalaune@orange.com>
2025-05-28 16:31:40 +0200

    add gd to php-runner

--- .scripts/push-base-images.sh
+++ .scripts/push-base-images.sh
@@ -51,7 +51,7 @@ build_and_push_to_github "node-tools-22" "node-tools:22-bullseye-slim" "node-too
 # Buildx Images --platform linux/amd64,linux/arm64/v8
 DOCKER_PLATFORMS="--platform linux/amd64,linux/arm64/v8"
 build_and_push_to_github "php-8.3.13" "php:8.3.13-apache-bookworm"
-build_and_push_to_github "php-runner-8.3.13" "php-runner:8.3.13-apache-bookworm" "php-runner:8.3.13-03"
+build_and_push_to_github "php-runner-8.3.13" "php-runner:8.3.13-apache-bookworm" "php-runner:8.3.13-04"
 build_and_push_to_github "php-tools-8.3.13" "php-tools:8.3.13-apache-bookworm" "php-tools:8.3.13-03"
 build_and_push_to_github "python-3.13" "python:3.13-slim-bookworm"
 build_and_push_to_github "python-tools-3.13" "python-tools:3.13-slim-bookworm"
